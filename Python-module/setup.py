aa6316a7e3e716ae20366ab90bbeeae569fb5a18
Mena-SA-Kamel <mena.kamel@sanofi.com>
2024-06-19 13:24:00 +0000

    Updated the setup.py to include a long description

--- Python-module/setup.py
+++ Python-module/setup.py
@@ -2,12 +2,22 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 HERE = Path(__file__).parent
+ld = """ # SpatialDE
+
+This package wraps SpatialDE so it can be installed and used using PyPI after fixing some bugs.
+
+All SpatialDE credit goes to the SpatialDE authors. Please check the original publication for more details: [Nature Methods](https://www.nature.com/articles/nmeth.4636).
+
+If you use this package, please cite the original SpatialDE publication.
+
+ """
 
 setup(
     name='SpatialDE-SO',
     version='1.1.4',
     description='Spatial and Temporal DE test. This is a fork containing some bug fixes.',
-    long_description=(HERE.parent / 'README.rst').read_text(),
+    long_description=ld,
+    long_description_content_type='text/markdown',
     url='https://github.com/Mena-SA-Kamel/SpatialDE-SO',
     packages=find_packages(),
     include_package_data=True,
